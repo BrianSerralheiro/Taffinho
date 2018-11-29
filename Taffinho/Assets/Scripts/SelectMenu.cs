aed932e7e4be01d16b9c55921533819a14bfd344
Arthur Tavares <arth_tavares@hotmail.com>
2018-11-29 19:47:24 -0200

    Added the base for the scrips for the world select menu(WorldMenu) and sng selection menu(SelectMenu), both are just basic scripts and should be worked on

---
+++ Taffinho/Assets/Scripts/SelectMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectMenu : MonoBehaviour {
+	[SerializeField]
+	public MusicInfo[] song;
+    private string[] names;
+    // Use this for initialization
+    public GUIStyle style;
+	void Start () {
+        gameObject.SetActive(false);
+	}
+    private void OnGU()
+    {
+        int x = Screen.width / 10;
+        int y = Screen.height / 10;
+        for (int i = 0; i < song.Length; i++)
+        {
+            if (GUI.Button(new Rect(x * 3, y + i * y * 2, x * 4, y * 2), song[i].name, style))
+            {
+                NotePosition.music = song[i];
+                SceneManager.LoadScene(1);
+            }
+        }
+
+    }
+    // Update is called once per frame
+    public string[] Names()
+    {
+        //if(names==null || names.Length == 0)
+        {
+            names = new string[song.Length];
+            for(int i = 0; i < song.Length; i++)
+            {
+                if (song[i].name != "" && song[i].name != null) names[i] = song[i].name;
+                else names[i] = "Vazio"+(i+1);
+            }
+        }
+        return names;
+    }
+    void Update () {
+		
+	}
+}
---
+++ Taffinho/Assets/Scripts/WorldMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldMenu : MonoBehaviour {
+    public SelectMenu[] Worlds;
+    public bool selecting;
+    public GUIStyle style;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnGUI()
+    {
+        int x = Screen.width / 10;
+        int y = Screen.height / 10;
+        if (selecting)
+        {
+            if (GUI.Button(new Rect(x, y, x, y), "Back"))
+            {
+                selecting = false;
+                for (int i = 0; i < Worlds.Length; i++)
+                {
+                    Worlds[i].gameObject.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            GUI.Box(new Rect(x*3, y, x*4, y*2), "Chose World");
+            for (int i = 0; i < Worlds.Length;i++)
+            {
+                if (GUI.Button(new Rect(x*3, y*3 + i * y*2, x*4, y*2), Worlds[i].name, style))
+                {
+                    selecting = true;
+                    Worlds[i].gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+    public void Select(int i)
+    {
+        for(int j = 0; j < Worlds.Length; j++)
+        {
+            if (Worlds[j].gameObject.activeSelf)
+            {
+                NotePosition.music = Worlds[j].song[i];
+                SceneManager.LoadScene(1);
+                return;
+            }
+        }   
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
