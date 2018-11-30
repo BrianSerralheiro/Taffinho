5729d9db4f51acc2be1a05773a3548b507784e5a
Yuri Katayama <tooketsu@gmail.com>
2018-11-30 16:13:23 -0200

    Guitar Texture Offset

---
+++ Taffinho/Assets/Scripts/GuitarMaterialOffset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuitarMaterialOffset : MonoBehaviour {
+
+	private Renderer rendy;
+	public float speed;
+	private float offset;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		rendy = GetComponent<Renderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		offset = 0.01f * Time.time * speed;
+		rendy.material.SetTextureOffset ("_MainTex", new Vector2 (0, -offset));
+	}	
+}
