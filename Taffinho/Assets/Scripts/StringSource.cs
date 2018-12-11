a74f4c9fa6e5ca34fc67717a3ca0925f002fefe6
Yuri Katayama <tooketsu@gmail.com>
2018-12-11 13:14:13 -0200

    Strings Class + Tests
    Created the StringSource class tha will contain all strings of the project, including displayed strings.

---
+++ Taffinho/Assets/Scripts/StringSource.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main Strings Class
+/// Public Static class that contains all in-game texts and strings
+/// Call by StringContainner.StringName to get it's values
+/// </summary>
+
+public static class StringSource {
+
+/// <sumary>
+/// All Menu Strings Here
+/// </sumary>
+
+public static string menuStart		= "Começar Jogo";
+public static string menuTutorial	= "Como Jogar";
+public static string menuOptions	= "Opções";
+public static string menuCredts		= "Créditos";
+
+/// <summary>
+/// Song Names
+/// </summary>
+public static string song1 			= "Nome da Musica1";
+public static string song2 			= "Nome da Musica2";
+public static string song3 			= "Nome da Musica3";
+
+/// <summary>
+/// Game Strings 
+/// </summary>
+public static string scoreText		= "Score: ";
+
+}
---
+++ Taffinho/Assets/Scripts/Teste.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour {
+
+	float tx;
+	float ty;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tx = Screen.width;
+		ty = Screen.height;
+		
+	}
+
+	void OnGUI() {
+
+		GUI.Box (new Rect(tx/2, ty/20, tx/10, ty/10), StringSource.scoreText);
+
+	}
+}
