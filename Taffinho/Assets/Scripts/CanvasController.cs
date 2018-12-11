51bab220a1f3def50a461cd4a04fb2e417705dc6
Yuri Katayama <tooketsu@gmail.com>
2018-12-11 14:53:16 -0200

    Documentation update
    Canvas Contrller documentation

--- Taffinho/Assets/Scripts/CanvasController.cs
+++ Taffinho/Assets/Scripts/CanvasController.cs
@@ -4,22 +4,40 @@ using System.Collections;
 
 public class CanvasController : MonoBehaviour
 {
-	public static MainMenu mainMenu;
 
-	public static WorldMenu worldMenu;
+	/// <summary>
+	/// Canvas Controller Class
+	/// Here every canvas/menu will be declared and change it's enable attribute to true or false
+	/// </summary>
+	/// <param name="MainMenu">Object of Main Menu</param>
+	/// <param name="WorldMenu">Object of World Menu</param>
+	/// <param name="defaultMenu">Integer used to change menus at switchMenu </param>
+	
+	public static MainMenu 		mainMenu;
+	public static WorldMenu 	worldMenu;
+	public static int 			defaultMenu = 0;
 
-	public static int defaultMenu = 0;
+	/// <summary>
+	/// Initialization of Menu Objects
+	/// Get the component of the object as it
+	/// </summary>
 
 	void Start ()
 	{
-		mainMenu = GetComponent<MainMenu> () as MainMenu;
-		worldMenu = GetComponent<WorldMenu> () as WorldMenu;
-		switchMenu(defaultMenu);
+		mainMenu 	= GetComponent<MainMenu> () as MainMenu;
+		worldMenu 	= GetComponent<WorldMenu> () as WorldMenu;
+		switchMenu	(defaultMenu);
 	}
+
+	/// <summary>
+	/// Method that will change Menu/Canvas objects
+	/// enable states to true/false according to it's number compared to id param
+	/// </summary>
+	/// <param name="id">Param used to compare itself to menu magic number</param>
 
 	public void switchMenu(int id)
 	{
-		mainMenu.enabled = 0 == id;
-		worldMenu.enabled = 1 == id;
+		mainMenu.enabled 	= 0 	== id;
+		worldMenu.enabled 	= 1 	== id;
 	}
 }
