89f5525b5dea61ea92feb6c049e769c47101b3c8
Arthur Tavares <arth_tavares@hotmail.com>
2018-12-14 12:19:47 -0200

    Added Script to control background musing in the main mennu

---
+++ Taffinho/Assets/Scripts/AutoVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof( AudioSource))]
+public class AutoVolume : MonoBehaviour {
+	AudioSource source;
+	// Use this for initialization
+	void Start () {
+		source=GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float volume=1;
+		if(source.time<1)volume=source.time;
+		else if(source.time>source.clip.length-1)volume=source.clip.length-source.time;
+		source.volume=volume;
+	}
+}
